60f83886974e1e6051ed8148330d0c09ec0c5c84
Mathieu Gauthron <mathieu@matlux.net>
2012-03-27 21:01:26 +0100

    first working example of hello world. Just go onto resources/public/index.html

--- project.clj
+++ project.clj
@@ -1,5 +1,12 @@
 (defproject clojure-game-of-life-dojo "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :description "A simple example of how to use lein-cljsbuild"
+  :source-path "src-clj"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[lein-ring "0.5.4"]]
   :plugins [[lein-cljsbuild "0.1.3"]]
-)
+  :cljsbuild {
+    :builds [{:source-path "src-cljs"
+              :compiler {:output-to "resources/public/js/main.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}]} )
+
